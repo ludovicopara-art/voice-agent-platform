0ac5b20776b9f4130f758f43dff90c82755d16b8
ludovicopara-art <ludovicopara@gmail.com>
2026-09-19 16:08:43 +0200

    Create calendarService.js

---
+++ src/calendarService.js
@@ -0,0 +1,49 @@
+// src/calendarService.js
+const { google } = require("googleapis");
+const { decrypt } = require("./crypto");
+
+function getOAuthClientForTenant(tenant) {
+    if (!tenant.googleRefreshTokenEnc) {
+    throw new Error(`Tenant ${tenant.id} non ha ancora collegato Google Calendar`);
+    }
+  const oauth2Client = new google.auth.OAuth2(
+    process.env.GOOGLE_OAUTH_CLIENT_ID,
+    process.env.GOOGLE_OAUTH_CLIENT_SECRET
+  );
+  oauth2Client.setCredentials({ refresh_token: decrypt(tenant.googleRefreshTokenEnc) });
+  return oauth2Client;
+}
+
+async function getCalendarClient(tenant) {
+  const auth = getOAuthClientForTenant(tenant);
+  return google.calendar({ version: "v3", auth });
+}
+
+async function eventiNellaFascia(tenant, inizioISO, fineISO) {
+  const calendar = await getCalendarClient(tenant);
+  const res = await calendar.events.list({
+        calendarId: tenant.googleCalendarId || "primary",
+        timeMin: inizioISO,
+        timeMax: fineISO,
+        singleEvents: true,
+    });
+  return res.data.items || [];
+}
+
+async function creaEvento(tenant, { summary, description, inizioISO, fineISO, extendedProps }) {
+  const calendar = await getCalendarClient(tenant);
+  const evento = await calendar.events.insert({
+        calendarId: tenant.googleCalendarId || "primary",
+        requestBody: {
+          summary,
+          description,
+          start: { dateTime: inizioISO, timeZone: tenant.timezone },
+          end: { dateTime: fineISO, timeZone: tenant.timezone },
+          extendedProperties: { private: extendedProps || {} },
+    },
+    });
+      return evento.data;
+    }
+
+    module.exports = { eventiNellaFascia, creaEvento };
+    
